9e23babf1211523c288b13d22ff9c34791983b35
liu kelin <314566990@qq.com>
2017-09-13 17:30:09 +0800

    同步到redis demo

--- python_sync_nosql/sync_redis.py
+++ python_sync_nosql/sync_redis.py
@@ -4,7 +4,7 @@
 
 '''
 
- 消费 rabbitmq 数据 同步到redis
+ 消费 数据 写入到redis
 
 '''
 import os
@@ -20,29 +20,64 @@ def conn_redis():
 	print(redisConn, redisConn.ping())
 
 '''
- 写入到redis
+ ·将数据写入到redis
+  这里的demo是将表数据映射到 redis hash 结构，key=db:table:primary_id
+  body={
+	    "binlog": "mysql-bin.000009:1235",
+	    "db": "test",
+	    "table": "users",
+	    "eventType": "UPDATE",
+	    "before": {
+	        "uid": "8",
+	        "username": "duobao153713223"
+	    },
+	    "after": {
+	        "uid": "8",
+	        "username": "duobao153713223"
+	    },
+	    "time": "2016-08-22 17:47:25"
+	}
 '''
-def set_redis(body):
+def set_data(body):
 	if not body or body=='':
 		return False
 	data = json.loads(body)
 	if isinstance(data, (dict)) == False:
 		return False
 
+
 	if data.has_key('eventType') and data.has_key('after') and data.has_key('db') and data.has_key('table'):
+		redis_cache_map = config.get('redis_cache_map')
 
 		if redis_cache_map.has_key(data['db']) and redis_cache_map[data['db']].has_key(data['table']):
-			key = redis_cache_map[data['db']][data['table']]
+			key = "%s_%s_%s" %(data['db'], data['table'], redis_cache_map[data['db']][data['table']])
 		else:
 			return False
 
-		if data['eventType'] in ['UPDATE', 'INSERT', 'DELETE'] and isinstance(data['after'], (dict)):
-			try:
-				redisConn.hmset(key, data['after'])
-			except:
+		if data.get('eventType') in ['UPDATE', 'INSERT', 'DELETE'] and isinstance(data['after'], (dict)):
+			
+			if not redisConn:
 				conn_redis()
+
+			if data.get('eventType')=='INSERT':
+
 				redisConn.hmset(key, data['after'])
+
+			else if data.get('eventType')=='UPDATE':
+
+				redisConn.hmset(key, data['after'])
+
+			else if data.get('eventType')=='DELETE':
+
+				redisConn.del(key)
+
+			# try:
+			# 	redisConn.hmset(key, data['after'])
+			# except:
+			# 	conn_redis()
+			# 	redisConn.hmset(key, data['after'])
 			return True
+
 	return False
 
 
